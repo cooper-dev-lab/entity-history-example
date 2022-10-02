cb8ea3dc643da53de63d6c7a64a439c95875f632
cooper <pbg0205@naver.com>
2022-10-03 00:14:29 +0900

    refactor: 미사용 import 제거

--- src/main/java/com/example/entityhistoryexample/application_event_publisher/service/AccountHistoryServiceV1.java
+++ src/main/java/com/example/entityhistoryexample/application_event_publisher/service/AccountHistoryServiceV1.java
@@ -6,7 +6,6 @@ import com.example.entityhistoryexample.dto.AccountHistoryRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
