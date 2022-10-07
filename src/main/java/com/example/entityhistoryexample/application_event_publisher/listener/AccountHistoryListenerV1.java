e4a8f8e3992e370b9e53cebf9025358574605558
cooper <pbg0205@naver.com>
2022-10-07 18:26:27 +0900

    feat: @Async annotation 추가
    - ApplicationEventPublisher 는 동기로 동작하기 때문에 @Async annotation 을 통해 비동기로 동작하도록 변경

--- src/main/java/com/example/entityhistoryexample/application_event_publisher/listener/AccountHistoryListenerV1.java
+++ src/main/java/com/example/entityhistoryexample/application_event_publisher/listener/AccountHistoryListenerV1.java
@@ -4,6 +4,7 @@ import com.example.entityhistoryexample.application_event_publisher.domain.Accou
 import com.example.entityhistoryexample.dto.AccountHistoryRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.scheduling.annotation.Async;
 
 import javax.persistence.PostPersist;
 import javax.persistence.PostUpdate;
@@ -14,6 +15,7 @@ public class AccountHistoryListenerV1 {
     @Autowired
     private ApplicationEventPublisher applicationEventPublisher;
 
+    @Async
     @PostPersist
     public void postPersist(AccountV1 account) {
         AccountHistoryRequestDto accountHistoryRequestDto = AccountHistoryRequestDto.create(
@@ -25,6 +27,7 @@ public class AccountHistoryListenerV1 {
         applicationEventPublisher.publishEvent(accountHistoryRequestDto);
     }
 
+    @Async
     @PostUpdate
     public void postUpdate(AccountV1 account) {
         AccountHistoryRequestDto accountHistoryRequestDto = AccountHistoryRequestDto.create(
